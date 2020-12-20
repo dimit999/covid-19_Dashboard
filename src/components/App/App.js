73399dc1f07aa661f1c5d2ff27aad45170db8936
NedVill <liamholwett@gmail.com>
2020-12-20 18:43:17 +0300

    fix: fix chart comment

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -52,7 +52,7 @@ export default class App {
         this.currentCountry = data.country;
         this.updateCountryInput();
         this.ConditionalListing.update(this.currentCountry);
-        // this.chart.update();
+        this.chart.update();
       }
     });
   }
