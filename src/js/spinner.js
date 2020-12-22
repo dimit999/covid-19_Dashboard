564e5be6d3805ceaa41a4a34f8c658d9ac31a17e
d.ananyev <d.ananyev@a1qa.com>
2020-12-22 22:53:18 +0400

    feat: add confirm

--- src/js/spinner.js
+++ src/js/spinner.js
@@ -1,16 +1,10 @@
 document.onreadystatechange = () => {
   if (document.readyState !== 'complete') {
     document.querySelector('body').style.visibility = 'hidden';
-    document.querySelector('body').style.background = 'black';
+    document.querySelector('body').style.backgroundColor = 'black';
     document.querySelector('#loading').style.visibility = 'visible';
   } else {
     document.querySelector('#loading').style.display = 'none';
     document.querySelector('body').style.visibility = 'visible';
-    document.querySelector('body').style = `background-image: url('./assets/images/jpeg/bg.jpeg');
-    background-repeat: no-repeat center fixed;
-    background-size: cover;
-    background-position-y: no-repeat center;
-    background-position-x: no-repeat;
-    color: $color-white;`;
   }
 };
